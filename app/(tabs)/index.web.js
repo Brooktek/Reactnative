35186fadfed1c730948e83fb218527c717bfdea0
Brook Tekle (Opshadow) <126861167+Brooktek@users.noreply.github.com>
2025-04-22 22:09:26 +0300

    update

--- app/(tabs)/index.web.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-import { registerRootComponent } from 'expo';
-
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  rootTag: document.getElementById('root'),
-});
